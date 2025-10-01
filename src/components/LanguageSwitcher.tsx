d7ba5d19890c8cdf3e43c5f0ee0de7b01d0c524d
aisha-viganium <aisha.tark@viganium.com>
2025-10-01 17:32:05 +0300

    translation

--- src/components/LanguageSwitcher.tsx
+++ src/components/LanguageSwitcher.tsx
@@ -19,11 +19,46 @@ export default function LanguageSwitcher() {
   };
 
   return (
-    <button
-      onClick={toggleLocale}
-      className="px-3 py-1 rounded bg-blue-500 text-white"
-    >
-      {currentLocale === "ar" ? "English" : "العربية"}
-    </button>
+    <>
+
+      <div  style={{
+                    backgroundColor: "#FFF",
+                    border: `2px solid #FFF`,
+
+                    outline: `2px solid #FFF`,
+
+                }}
+                className="
+                cursor-pointer flex flex-row justify-center items-center my-5
+                px-0 py-[2px] gap-[6.24px] rounded-[4px] font-bold text-[10px] leading-[14px]
+                md:py-[17px] md:gap-[10px] md:w-[188px] md:h-[63px] 
+                md:rounded-[16px] md:text-[20px] md:leading-[29px]
+                w-[80px] h-[30px] 
+                text-center 
+                transition-all duration-300 ease-in-out
+                hover:outline-offset-4
+            ">
+        <button
+          onClick={() => toggleLocale()}
+          className={` cursor-pointer w-[75px] h-[50px] flex items-center justify-center rounded-lg font-semibold text-[16px] ${
+            currentLocale === "ar"
+              ? "bg-[#BD171D] text-white"
+              : "bg-transparent text-[#414141]"
+          }`}
+        >
+          عربي
+        </button>
+        <button
+          onClick={() => toggleLocale()}
+          className={`cursor-pointer w-[75px] h-[50px] flex items-center justify-center rounded-lg font-semibold text-[16px] ${
+            currentLocale === "en"
+              ? "bg-[#BD171D] text-white"
+              : "bg-transparent text-[#414141]"
+          }`}
+        >
+          English
+        </button>
+      </div>
+      </>
   );
 }
--- src/layout/nav-bar/blocks/NavBarActions.tsx
+++ src/layout/nav-bar/blocks/NavBarActions.tsx
@@ -1,11 +1,27 @@
+"use client";
+import LanguageSwitcher from "@/components/LanguageSwitcher";
+// import { useState } from "react";
 import PoPUpChat from "@/components/PoPUpChat";
+import { useTranslation } from "react-i18next";
 
 export default function NavBarActions() {
+  // const { i18n } = useTranslation();
+  // const [lang, setLang] = useState(i18n.language || "ar");
+
+  // const changeLang = (lng: "ar" | "en") => {
+  //   setLang(lng);
+  //   i18n.changeLanguage(lng);
+  // };
+
   return (
-    <div className="flex items-center">
+    <div className="flex items-center gap-3">
+    <LanguageSwitcher />
+
       <div className="flex items-center gap-2 pe-3">
-        <PoPUpChat bg ="#FFF" color ="#1A1A1A" border="#F6EDE0" />
+        <PoPUpChat bg="#FFF" color="#1A1A1A" border="#F6EDE0" />
       </div>
+
+
     </div>
   );
 }
